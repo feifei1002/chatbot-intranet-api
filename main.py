1ae59faf707ad03a01d9d749a89e50fd0028ba62
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-02-19 15:24:00 +0000

    added new python file to test asking question to openai and getting response as a message to /utils/test

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from utils import suggested_questions
 
 app = FastAPI()
-
+app.include_router(suggested_questions.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
 )
-
 
 @app.get("/")
 async def root():
---
+++ utils/suggested_questions.py
@@ -0,0 +1,33 @@
+import os
+
+from fastapi import APIRouter
+from openai import AsyncOpenAI
+
+router = APIRouter()
+
+TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
+
+client = AsyncOpenAI(api_key=TOGETHER_API_KEY,
+                base_url='https://api.together.xyz',
+                )
+
+# test input
+test_input = """What is a large language model?"""
+
+@router.get("/utils/test")
+async def suggestions_test():
+    resp = await client.chat.completions.create(
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": test_input
+            }
+        ]
+    )
+
+    return {"message": resp.choices[0].message.content}
