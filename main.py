27e99315353606460e413a5b66c34e36d0368baa
c21081270 <LiuF29@cardiff.ac.uk>
2024-02-20 21:10:27 +0000

    added duckduckgo.py to main.py and include a route for it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from utility.duckduckgo import duckduckgo_search
+
 app = FastAPI()
 
 app.add_middleware(
@@ -12,3 +14,9 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/chatbot-uni")
+async def duckduckgosearch(query: str):
+    search_results = duckduckgo_search(query)
+    return {"search_results": search_results}
