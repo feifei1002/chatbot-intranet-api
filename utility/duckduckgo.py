ee404bacea50118f69c0e82eb22e98d9429c1cde
c21081270 <LiuF29@cardiff.ac.uk>
2024-02-20 21:10:27 +0000

    added duckduckgo.py to main.py and include a route for it

--- utility/duckduckgo.py
+++ utility/duckduckgo.py
@@ -3,25 +3,28 @@ from langchain_community.document_transformers import Html2TextTransformer
 from langchain.tools import DuckDuckGoSearchResults
 import re
 
-search = DuckDuckGoSearchResults(source="cardiff.ac.uk")
 
-# This should take in user input later
-result = search.run("applied software engineering site:cardiff.ac.uk")
+def duckduckgo_search(query):
+    search = DuckDuckGoSearchResults(source="cardiff.ac.uk")
 
-# get rid of '[' and ']' to extract the link easier
-clean_result = result.replace('[', '').replace(']', '')
-links = re.findall(r'link: (.*?),', clean_result)
-print(links)
+    # This should take in user input later
+    result = search.run(query)
 
-# Uses AsyncHtmlLoader to make asynchronous HTTP requests to fetch the data
-loader = AsyncHtmlLoader(links)
-data = loader.load()
+    # get rid of '[' and ']' to extract the link easier
+    clean_result = result.replace('[', '').replace(']', '')
+    links = re.findall(r'link: (.*?),', clean_result)
+    print(links)
 
-# Uses HTML2Text to convert HTML content into plain text
-html2text = Html2TextTransformer()
-data_transformed = html2text.transform_documents(data)
+    # Uses AsyncHtmlLoader to make asynchronous HTTP requests to fetch the data
+    loader = AsyncHtmlLoader(links)
+    data = loader.load()
 
-# Remove all the new line (\n) to clean up
-for doc in data_transformed:
-    cleanup_data = doc.page_content.replace('\n', '')
-    print(cleanup_data)
+    # Uses HTML2Text to convert HTML content into plain text
+    html2text = Html2TextTransformer()
+    data_transformed = html2text.transform_documents(data)
+
+    # Remove all the new line (\n) to clean up
+    for doc in data_transformed:
+        cleanup_data = doc.page_content.replace('\n', '')
+        print(cleanup_data)
+        return cleanup_data
