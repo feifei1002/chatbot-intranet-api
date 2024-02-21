1f263d5203736009e5bb34252deeae64958b9a5b
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-02-21 16:31:42 +0000

    Get request sends JSON response of 3 suggested questions.

--- utils/suggested_questions.py
+++ utils/suggested_questions.py
@@ -1,7 +1,8 @@
 import os
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from openai import AsyncOpenAI
+import json
 
 router = APIRouter()
 
@@ -11,7 +12,7 @@ client = AsyncOpenAI(api_key=TOGETHER_API_KEY,
                      base_url='https://api.together.xyz', )
 
 # specifically set question to start with
-first_question = """What is the breed of the largest cat?"""
+first_question = "What is the breed of the largest cat?"
 
 # data sent to openai
 input_messages = [
@@ -21,27 +22,35 @@ input_messages = [
 
 
 # function to ask question to openai and get response
-@router.get("/utils/test")
-async def ask_questions():
-    # send response to api
-    resp = await client.chat.completions.create(
+@router.get("/suggested")
+async def suggested_questions():
+    # test conversation to check the api can review this
+    # conversation and create suggested qs from it
+    await client.chat.completions.create(
         model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         messages=input_messages
     )
 
-    await suggest_questions(
-        "In the format of a JSON array, what are 3 good questions to ask after this conversation")
+    # gets 3 suggested questions as json array
+    suggested_qs = await json_suggestions(
+        "In only the format of a JSON array, "
+        "what are 3 good questions to ask after this conversation?"
+        "but do not explain reasoning for each question.")
+    # makes sure file format is correct
+    json_suggested_qs = json.dumps(suggested_qs, default=str)
+
     # sends response to page
-    return {"message": resp.choices[0].message.content}
+    return Response(content=json_suggested_qs, media_type='application/json')
 
 
-async def suggest_questions(suggest):
+async def json_suggestions(suggest):
     # potential issue with continuous appending to list of dictionaries
-    input_messages.append(
+    previous_messages = input_messages
+    previous_messages.append(
         {"role": "user", "content": suggest})
 
     resp = await client.chat.completions.create(
         model="mistralai/Mixtral-8x7B-Instruct-v0.1",
-        messages=input_messages
+        messages=previous_messages
     )
-    print(resp.choices[0].message.content)
+    return resp.choices[0].message.content
