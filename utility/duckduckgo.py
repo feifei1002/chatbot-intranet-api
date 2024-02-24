db23b5e6db8a0e9aed109b91604724b79c7dc5ca
c21081270 <LiuF29@cardiff.ac.uk>
2024-02-24 22:55:17 +0000

    solved RateLimitError, working on fetching more accurate data

--- utility/duckduckgo.py
+++ utility/duckduckgo.py
@@ -1,8 +1,9 @@
 import os
 import time
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import AsyncHtmlLoader
 from langchain_community.document_transformers import Html2TextTransformer
-from langchain.tools import DuckDuckGoSearchResults
+from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
@@ -15,16 +16,16 @@ import re
 
 TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
 client = OpenAI(api_key=TOGETHER_API_KEY, base_url="https://api.together.xyz/v1")
+embedding = TogetherEmbeddings(model= "togethercomputer/m2-bert-80M-8k-retrieval")
 
 
 async def duckduckgo_search(query):
-    # wrapper = DuckDuckGoSearchAPIWrapper(max_results=5)
-    search = DuckDuckGoSearchResults(source="site:cardiff.ac.uk")
+    wrapper = DuckDuckGoSearchAPIWrapper(max_results=1, region="uk-en")
+    search = DuckDuckGoSearchResults(api_wrapper=wrapper, query=query)
 
     # This should take in user input later
     result = search.run(query)
     print(result)
-    r'(https?://[^\s]+)'
 
     # get rid of '[' and ']' to extract the link easier
     clean_result = result.replace('[', '').replace('],', '').replace(']', '')
@@ -32,20 +33,23 @@ async def duckduckgo_search(query):
     print(links)
     data_transformed_list = []
     # Uses AsyncHtmlLoader to make asynchronous HTTP requests to fetch the data
-    for link in links:
-        loader = AsyncHtmlLoader(link)
-        data = loader.load()
+    # for link in links:
+    loader = AsyncHtmlLoader(links)
+    data = loader.load()
 
-        # Uses HTML2Text to convert HTML content into plain text
-        html2text = Html2TextTransformer()
-        data_transformed = html2text.transform_documents(data)
-        print("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬")
-        data_transformed[0].page_content[0:1000].replace(' \n', '').replace('\r', '')
-        data_transformed_list.extend(data_transformed)
-    vectorstore = FAISS.from_documents(data_transformed_list[:1],
-                                       TogetherEmbeddings(model="togethercomputer/m2-bert-80M-8k-retrieval"))
+    # Uses HTML2Text to convert HTML content into plain text
+    html2text = Html2TextTransformer()
+    data_transformed = html2text.transform_documents(data)
+    data_transformed[0].page_content.replace(' \n', '').replace('\r', '')
+    data_transformed_list.extend(data_transformed)
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=0,
+        separator="\n"
+    )
+    documents = text_splitter.split_documents(data_transformed_list)
+    vectorstore = FAISS.from_documents(documents, TogetherEmbeddings(model="togethercomputer/m2-bert-80M-8k-retrieval"))
     retriever = vectorstore.as_retriever()
-    time.sleep(5)
     model = Together(
         model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         temperature=0.7,
@@ -70,32 +74,6 @@ async def duckduckgo_search(query):
 
     input_query = query
     output = chain.invoke(input_query)
+    print(output)
 
     return output
-    # found_search = store.similarity_search(
-    #     query=query,
-    #     k=3,
-    # )
-    # print("--------------------------")
-    # return found_search
-
-        # embeddings_list = []
-    # # Remove all the new line (\n) to clean up
-    # for doc in data_transformed:
-    #     cleanup_data = doc.page_content.replace(' \n', '').replace('\r', '')
-    #     print(cleanup_data)
-    #     embedding_data = \
-    #         client.embeddings.create(input=cleanup_data, model="togethercomputer/m2-bert-80M-32k-retrieval").data[
-    #             0].embedding
-    #     embeddings_list.append(embedding_data)
-    #
-    # embedding_query = client.embeddings.create(input=query, model="togethercomputer/m2-bert-80M-32k-retrieval").data[
-    #     0].embedding
-    # similar_search = []
-    # for embedding in embeddings_list:
-    #     similarity = cosine_similarity([embedding_query], [embedding])[0][0]
-    #     similar_search.append((query, similarity))
-    # similar_search.sort(key=lambda x: x[1], reverse=True)
-    # recommendations = [search for search, _ in similar_search]
-    # print("Recommendations:", recommendations)
-    # return recommendations
