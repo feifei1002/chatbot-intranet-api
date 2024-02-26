4bbbd5d7c0f010ebe836b38dd23088b131efa879
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-02-26 18:47:35 +0000

    Added basic test for /suggested page, and empty init file so pytest passes (but won't pass on gitlab because no env variable set?)

--- tests/test_suggested_qs.py
+++ tests/test_suggested_qs.py
@@ -10,3 +10,9 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+# test that /suggested loads with code 200
+def test_suggested_questions():
+    response = client.get("/suggested")
+    assert response.status_code == 200
