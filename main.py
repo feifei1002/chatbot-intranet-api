08754ffaf5da090db4401be3a46f44f6e03baa60
c21081270 <LiuF29@cardiff.ac.uk>
2024-02-27 11:11:17 +0000

    fixing pipeline errors

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from utility.scrapeUniWebsite import duckduckgo_search, transform_data, process_search_results
+from utility.scrapeUniWebsite import (duckduckgo_search,
+                                      transform_data,
+                                      process_search_results)
 
 app = FastAPI()
 
@@ -19,6 +21,6 @@ async def root():
 @app.get("/chatbot-uni")
 async def scrape_uni_website(query):
     search_links = await duckduckgo_search(query)
-    transformed_data = transform_data(search_links)
+    transform_data(search_links)
     response = process_search_results(query, search_links)
     return {"response": response}
