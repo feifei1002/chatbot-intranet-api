e61016ccf6e260d1c2b60db142a2b5305ca543db
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-02-28 12:39:16 +0000

    Attempted to add environment variable as variable in .gitlab-ci.yml, taking value from GitLab CI/CD

--- tests/test_suggested_qs.py
+++ tests/test_suggested_qs.py
@@ -4,7 +4,6 @@ from main import app
 # make sure add api key to env variables when running tests
 client = TestClient(app)
 
-
 # basic test for root page
 def test_root():
     response = client.get("/")
