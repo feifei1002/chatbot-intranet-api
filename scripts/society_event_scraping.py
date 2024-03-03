c524556bf096fcda7a5d26b3c08ae6bce7cdef2c
Dylan Silva <c21088964@cardiff.ac.uk>
2024-03-03 23:37:57 +0000

    code for retrieving society links

--- scripts/society_event_scraping.py
+++ scripts/society_event_scraping.py
@@ -5,13 +5,13 @@ import time
 
 
 def scrape_links():
-    # Launch the Chrome browser
+    # Set Chrome options to run headlessly
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Run in headless mode
     chrome_options.add_argument("--disable-gpu")  # Disable GPU usage
 
     # Launch the Chrome browser with headless options
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)  # Make sure chromedriver is in your PATH
 
     # Load the webpage
     driver.get("https://www.cardiffstudents.com/activities/societies/")
@@ -28,12 +28,14 @@ def scrape_links():
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(page_source, 'html.parser')
 
-    # Extract the desired content
+    # Extract the href attribute from the <a> tag within the #working-on div
     links = soup.select("#working-on a.msl-gl-link")
 
-    href_links = [link['href'] for link in links]
+    href_links = [f"https://www.cardiffstudents.com{link['href']}" for link in links]
 
-    print(href_links)
+    return href_links
 
 
-scrape_links()
+# Call the function and print the results
+scraped_links = scrape_links()
+print(scraped_links)
