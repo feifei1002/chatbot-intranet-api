cbb31a7932a96c15a98c08a206a4a03c5ca1707d
eye man ^^ <alia64@cardiff.ac.uk>
2024-03-12 14:45:00 +0000

    deepgram transcriber

---
+++ routes/deepgram_transcriber.py
@@ -0,0 +1,26 @@
+import os
+from deepgram import DeepgramClient, PrerecordedResponse, PrerecordedOptions
+from fastapi import APIRouter, Request
+
+router = APIRouter()
+deepgram = DeepgramClient(os.getenv("DEEPGRAM_API_KEY"))
+
+@router.post("/transcribe")
+async def transcribe(request: Request):
+    data = await request.body()
+
+    source = {
+        "buffer": data,
+        "mimetype": "audio/ogg"
+    }
+
+    resp: PrerecordedResponse = await deepgram.listen.asyncprerecorded.v("1").transcribe_file(
+        source,
+        PrerecordedOptions(
+            model="nova-2"
+        ),
+    )
+
+    return {
+        "text": resp.results.channels[0].alternatives[0].transcript
+    }
