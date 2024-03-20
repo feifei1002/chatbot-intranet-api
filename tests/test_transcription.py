54c45eb4717c56bbd68aa5bc8fc8853ede10ce39
eye man ^^ <alia64@cardiff.ac.uk>
2024-03-20 15:55:56 +0000

    transcription test

---
+++ tests/test_transcription.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_transcription():
+    with open('../transcription_test.wav', 'rb') as file:
+        audio_blob = file.read()
+
+    response = client.post(
+        "/transcribe",
+        data={audio_blob},
+        headers={'Content-Type': 'audio/wav'}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {'text': 'domain expansion infinite void'}
