10898af01fd4a7169f88c1a3c451d259843c1f58
eye man ^^ <alia64@cardiff.ac.uk>
2024-03-20 19:42:51 +0000

    moved .wav file

--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 def test_transcription():
 
-    file_name = 'gojo.wav'
+    file_name = '../gojo.wav'
     assert os.path.exists(file_name), f"File '{file_name}' does not exist."
 
     with open(file_name, 'rb') as file:
@@ -18,6 +18,5 @@ def test_transcription():
         data={audio_blob},
         headers={'Content-Type': 'audio/wav'}
     )
-
     assert response.status_code == 200
     assert response.json() == {'text': 'domain expansion infinite void'}
