b64d727e417017de452b5c3b1ccc56c8058df6c4
eye man ^^ <alia64@cardiff.ac.uk>
2024-03-23 03:17:25 +0000

    added comments

--- routes/deepgram_transcriber.py
+++ routes/deepgram_transcriber.py
@@ -2,19 +2,26 @@ import os
 from deepgram import DeepgramClient, PrerecordedResponse, PrerecordedOptions
 from fastapi import APIRouter, Request
 
+# Initialize the FastAPI router for creating API endpoints
 router = APIRouter()
 deepgram = DeepgramClient(os.getenv("DEEPGRAM_API_KEY"))
 
 
+# Asynchronous endpoint to transcribe audio content.
+# Accepts audio data in the request body, sends it to Deepgram for transcription, and returns the transcribed text.
+# param "request": The request object containing the body with audio data.
+# return: A dictionary with the transcribed text under the key 'text'.
 @router.post("/transcribe")
 async def transcribe(request: Request):
     data = await request.body()
 
+    # Specifying format and content for transcription
     source = {
         "buffer": data,
         "mimetype": "audio/ogg"
     }
 
+    # Transcribe the audio file using Deepgram's API
     resp: PrerecordedResponse = await (deepgram.listen
     .asyncprerecorded.v("1").transcribe_file(
         source,
@@ -23,6 +30,7 @@ async def transcribe(request: Request):
         ),
     ))
 
+    # Extract the transcription result from the response
     return {
         "text": resp.results.channels[0].alternatives[0].transcript
     }
--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -6,16 +6,22 @@ client = TestClient(app)
 
 
 def test_transcription():
+    # Checks if .wav file exists.
     file_name = 'gojo.wav'
     assert os.path.exists(file_name), f"File '{file_name}' does not exist."
 
+    # Reads .wav file and stores contents in "audio_blob"
     with open(file_name, 'rb') as file:
         audio_blob = file.read()
 
+    # Tests post request with "audio_blob" as content
     response = client.post(
         "/transcribe",
         content=audio_blob,
         headers={'Content-Type': 'audio/wav'}
     )
+
+    # Tests for successful response
     assert response.status_code == 200
+    # Tests for successful transcription
     assert response.json() == {'text': 'domain expansion infinite void'}
