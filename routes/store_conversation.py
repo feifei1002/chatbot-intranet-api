277cb7bf294a2de81dcfa512635b18700c90fd2b
Fei Liu <LiuF29@cardiff.ac.uk>
2024-03-26 04:32:46 +0700

    updated authentication.py, store_conversation.py, database schema (triple-programming with Kavin and Alex)

--- routes/store_conversation.py
+++ routes/store_conversation.py
@@ -1,14 +1,14 @@
 import json
 import os
-import re
+from typing import Annotated, Union, List
+from uuid import UUID
 
-import numpy as np
 from fastapi import APIRouter, HTTPException, Response, Depends
 from openai import AsyncOpenAI
+from psycopg.rows import dict_row
 from pydantic import BaseModel
-import psycopg
 
-from routes.authentication import AuthenticatedUser, get_current_user_optional
+from routes.authentication import AuthenticatedUser, get_current_user_optional, get_current_user
 from utils.db import pool
 from utils.models import ConversationMessage
 
@@ -34,14 +34,15 @@ class ConversationTitle(BaseModel):
     conversation_title: str
 
 
-async def create_conversation_title(message_history: list[dict]):
+async def create_conversation_title(message_history: list[dict]) -> str:
     messages = message_history.copy()
 
     # adds question prompt to ask for suggestions
-    messages.append(
-        {"role": "user",
-         "content": "Based on the conversation so far, what is a title to summarise this conversation? "  # noqa
-                    "Make sure to format in a JSON object with an array in the key 'title'."})  # noqa
+    messages.append({
+        "role": "user",
+        "content": "Based on the conversation so far, what is a title to summarise this conversation? "  # noqa
+                   "Make sure to format in a JSON object with an array in the key 'title'."  # noqa
+    })
 
     # gets response after asking openapi question
     resp = await client.chat.completions.create(
@@ -54,7 +55,7 @@ async def create_conversation_title(message_history: list[dict]):
 
     # print("title is ", resp.choices[0].message.content)
 
-    return resp.choices[0].message.content
+    return json.loads(resp.choices[0].message.content)["title"]
 
 
 # function to get conversation history
@@ -90,13 +91,13 @@ async def store_conversation(message_history: list, conversation_title: str, use
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
             await cur.execute(
-                "INSERT INTO conversations (conversation_title, username)"
+                "INSERT INTO conversations (title, username)"
                 " VALUES (%s, %s)",
                 # Only extract the title, remove everything before index 21
                 # and the last 7 indices of the title
-                (conversation_title[21:-7], username),
+                (conversation_title[100:], username),
             )
-            for message in message_history:
+            for idx, message in message_history:
                 await cur.execute(
                     "INSERT INTO conversation_messages (role, content) VALUES (%s, %s)",
                     (message['role'], message['content'])
@@ -116,69 +117,68 @@ async def store_conversation(message_history: list, conversation_title: str, use
 #             )
 
 
-# get conversation id for given title and username from database
-async def get_conversation_id(conversation_title: str, username: str):
-    conn = await pool.getconn()
-    try:
-        async with conn.cursor() as cursor:
-            query = "SELECT conversation_id FROM conversations WHERE conversation_title = %s AND username = %s"
-            await cursor.execute(query, (conversation_title,username))
-            rows = await cursor.fetchall()
-
-            # for row in rows:
-            #     print(row)
-
-            # return conversation id
-            conversation_id = int(re.sub(r'[^\w]', ' ', str(rows[0])))
-            # print("id is: ", conversation_id)
-
-            return conversation_id
-    except psycopg.Error as e:
-        print("Error executing SELECT statement:", e)
-
-
-# get message id from conversation id
-async def get_message_id(conversation_id: int):
-    conn = await pool.getconn()
-    try:
-        async with conn.cursor() as cursor:
-            query = "SELECT message_id FROM conversation_history WHERE conversation_id = %s"
-            await cursor.execute(query, (conversation_id,))
-            rows = await cursor.fetchall()
-
-            # return history message id
-            # doesn't work yet because conversation_history is empty
-            # message_id = int(rows[0])
-            # print("message id is: ", rows)
-            test_rows = (3,4)
-            return test_rows
-    except psycopg.Error as e:
-        print("Error executing SELECT statement:", e)
+# # get conversation id for given title and username from database
+# async def get_conversation_id(conversation_title: str, username: str):
+#     conn = await pool.getconn()
+#     try:
+#         async with conn.cursor() as cursor:
+#             query = "SELECT conversation_id FROM conversations WHERE conversation_title = %s AND username = %s"
+#             await cursor.execute(query, (conversation_title,username))
+#             rows = await cursor.fetchall()
+#
+#             # for row in rows:
+#             #     print(row)
+#
+#             # return conversation id
+#             conversation_id = int(re.sub(r'[^\w]', ' ', str(rows[0])))
+#             # print("id is: ", conversation_id)
+#
+#             return conversation_id
+#     except psycopg.Error as e:
+#         print("Error executing SELECT statement:", e)
 
 
+# # get message id from conversation id
+# async def get_message_id(conversation_id: int):
+#     conn = await pool.getconn()
+#     try:
+#         async with conn.cursor() as cursor:
+#             query = "SELECT message_id FROM conversation_history WHERE conversation_id = %s"
+#             await cursor.execute(query, (conversation_id,))
+#             rows = await cursor.fetchall()
+#
+#             # return history message id
+#             # doesn't work yet because conversation_history is empty
+#             # message_id = int(rows[0])
+#             # print("message id is: ", rows)
+#             test_rows = (3,4)
+#             return test_rows
+#     except psycopg.Error as e:
+#         print("Error executing SELECT statement:", e)
 
-# get role and content from message id
-async def get_message_contents(message_id: list[int]):
-    conn = await pool.getconn()
-    try:
-        rows = []
-        async with conn.cursor() as cursor:
-            for message in message_id:
-                query = "SELECT role, content FROM conversation_messages WHERE message_id = %s"
-                # conversation_history table empty so doesn't work yet
-                await cursor.execute(query, (message,))
-                rows.append(await cursor.fetchall())
 
-            # for row in rows:
-            #     print("role is: ", row[0][0])
-            #     print("content is: ", row[0][1])
-
-            # print(rows)
-            conversation = [{t[0]: t[1]} for sublist in rows for t in sublist]
-            print(conversation)
-            return conversation
-    except psycopg.Error as e:
-        print("Error executing SELECT statement:", e)
+# # get role and content from message id
+# async def get_message_contents(message_id: list[int]):
+#     conn = await pool.getconn()
+#     try:
+#         rows = []
+#         async with conn.cursor() as cursor:
+#             for message in message_id:
+#                 query = "SELECT role, content FROM conversation_messages WHERE message_id = %s"
+#                 # conversation_history table empty so doesn't work yet
+#                 await cursor.execute(query, (message,))
+#                 rows.append(await cursor.fetchall())
+#
+#             # for row in rows:
+#             #     print("role is: ", row[0][0])
+#             #     print("content is: ", row[0][1])
+#
+#             # print(rows)
+#             conversation = [{t[0]: t[1]} for sublist in rows for t in sublist]
+#             print(conversation)
+#             return conversation
+#     except psycopg.Error as e:
+#         print("Error executing SELECT statement:", e)
 
 
 @router.post("/store_conversation")
@@ -195,17 +195,63 @@ async def handle_store_conversation(messages: ChatHistory,
     # return JSONResponse(content={"message": "Conversation stored successfully"})
     return Response(content=message_history_title, media_type='application/json')
 
-@router.post("/get_conversation_from_title")
-async def handle_send_conversation(title: ConversationTitle,
-                                   user: AuthenticatedUser =
-                                   Depends(get_current_user_optional)):
-    username = user.username
-    conversation_title = title.conversation_title
 
-    # select values from databases using title and username
-    conversation_id = await get_conversation_id(conversation_title, username)
-    message_id = await get_message_id(conversation_id)
-    conversation_history = await get_message_contents(message_id)
+# @router.post("/get_conversation_from_title")
+# async def handle_send_conversation(title: ConversationTitle,
+#                                    user: AuthenticatedUser =
+#                                    Depends(get_current_user_optional)):
+#     username = user.username
+#     conversation_title = title.conversation_title
+#
+#     # select values from databases using title and username
+#     conversation_id = await get_conversation_id(conversation_title, username)
+#     message_id = await get_message_id(conversation_id)
+#     conversation_history = await get_message_contents(message_id)
+#
+#     # return conversation history
+#     return Response(content=json.dumps(conversation_history), media_type='application/json')
 
-    # return conversation history
-    return Response(content=json.dumps(conversation_history), media_type='application/json')
+class Conversation(BaseModel):
+    id: UUID
+    title: str
+
+
+@router.get("/conversations", response_model=List[Conversation])
+async def get_conversations(current_user: Annotated[
+    Union[AuthenticatedUser],
+    Depends(get_current_user)
+]):
+    async with pool.connection() as conn:
+        async with conn.cursor(row_factory=dict_row) as cur:
+            await cur.execute("SELECT id, title FROM conversations WHERE username = %s", (current_user.username,))
+            conversations = await cur.fetchall()
+
+            return conversations
+
+
+# class Message(BaseModel):
+
+
+@router.get("/conversations/{conversation_id}", response_model=List[ConversationMessage])
+async def get_conversation_history(conversation_id: UUID, current_user: Annotated[
+    Union[AuthenticatedUser],
+    Depends(get_current_user)
+]):
+    async with pool.connection() as conn:
+        async with conn.cursor(row_factory=dict_row) as cur:
+            # Check if the user and conversation id match from db
+            await cur.execute("SELECT 1 FROM conversations WHERE username = %s AND id = %s",
+                              (current_user.username, conversation_id))
+
+            if cur.fetchone() is None:
+                raise HTTPException(status_code=403, detail="You don't have access to this conversation")
+
+            # Fetch the message content, role and order by conversation_history(idx)
+            # and return a dict for converting to List[ConversationMessage]
+            await cur.execute("SELECT messages.content, messages.role FROM conversation_history "
+                              "JOIN messages ON message_id = id "
+                              "WHERE conversation_id = %s ORDER BY conversation_history.idx", (conversation_id,))
+
+            history = await cur.fetchall()
+
+            return history
