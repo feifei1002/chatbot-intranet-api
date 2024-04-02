b00e77f5211983765d55a66a290886d31ac00c7a
Fei Liu <liuf29@cardiff.ac.uk>
2024-04-02 20:23:39 +0000

    fixed lint

--- routes/conversations.py
+++ routes/conversations.py
@@ -152,7 +152,8 @@ async def add_messages(messages: ChatHistory,
                        ]):
     # only generate a title when there are no values in conversation_history for the id
     generate_title = False
-
+    
+    username = current_user.username
     # insert into conversation_history and messages
     async with pool.connection() as conn:
         async with conn.transaction():
