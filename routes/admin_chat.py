8d13770965e8811f36b7da896709e95559d0e67a
Fei Liu <LiuF29@cardiff.ac.uk>
2024-04-11 22:55:25 +0700

    modified so that only one single route is used for asking questions

--- routes/admin_chat.py
+++ routes/admin_chat.py
@@ -1,6 +1,6 @@
-
 import os
 
+from pydantic import BaseModel
 from utils.db import pool
 from fastapi import APIRouter
 import anthropic
@@ -10,17 +10,21 @@ ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
 client = anthropic.Anthropic(api_key=ANTHROPIC_API_KEY)
 
 
+class Question(BaseModel):
+    content: str
+
+
 async def ask_claude(query, schema):
-    prompt = f"""You are an AI assistant that helps 
+    prompt = f"""<s>You are an AI assistant that helps 
     the admins of Cardiff University's chatbot to get some analytics.
-    Here is the schema for a database:
+    Here is the schema for a database:</s>
 
-    {schema}
+    <p>{schema}</p>
 
-    Given this schema, can you generate some analytics for the admins based on the following question?
-    Do not provide the SQL query, just the analytics.
+    <s>Given this schema, can you generate some analytics for the admins based on the following question?
+    Do not provide the SQL query, just the analytics.</s>
 
-    Question: {query}
+    <m>Question: {query}</m>
     """
     return prompt
 
@@ -37,39 +41,14 @@ async def admin_chat(question):
     response = client.messages.create(
         model="claude-3-opus-20240229",
         max_tokens=4096,
-        messages=[question],
+        messages=[{"role": "user", "content": question}],
         system=prompt
     )
     print("Response:", response.content[0].text)
     return response.content[0].text
 
 
-@router.get("/10_most_asked_questions")
-async def get_10_most_asked_questions():
-    question = {
-        "role": "user",
-        "content": "What are the top 10 most asked questions in general?"
-    }
-    response = await admin_chat(question)
-    return response
-
-
-@router.get("/5_most_asked_questions_uni_website")
-async def get_5_most_asked_questions_uni_website():
-    question = {
-        "role": "user",
-        "content": "What are the 5 most asked questions "
-                   "related to the University's website?"
-    }
-    response = await admin_chat(question)
-    return response
-
-
-@router.get("/5_most_asked_questions_student_life")
-async def get_5_most_asked_questions_intranet():
-    question = {
-        "role": "user",
-        "content": "What are the 5 most asked questions related to the student life?"
-    }
-    response = await admin_chat(question)
+@router.post("/admin_chat")
+async def ask_question(question: Question):
+    response = await admin_chat(question.content)
     return response
