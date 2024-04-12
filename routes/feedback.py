b76048dcbd2adeec1446e8eac07cc1c4161eb1b6
Ayman Ali <alia64@cardiff.ac.uk>
2024-04-12 12:26:49 +0100

    added text feedback field in table

--- routes/feedback.py
+++ routes/feedback.py
@@ -13,10 +13,11 @@ async def transcribe(request: Request):
     data = await request.body()
     message = data.get("message")
     is_positive = data.get("positive")
+    feedback = data.get("feedback")
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("SELECT id FROM messages WHERE content = %s", (message,))
-            message_id = (await cur.fetchone())
-            await cur.execute("INSERT INTO feedback (message_id, is_positive) VALUES (%s, %s)", (message_id, is_positive))
-            # hello ayman, is_positive might be a string and it might not work since the table expects a boolean
+            await cur.execute("INSERT INTO feedback (message_id, is_positive, written_feedback) "
+                              "VALUES (SELECT id FROM messages WHERE content = %s), %s, %s)",
+                              (message, is_positive, feedback),)
+
 
