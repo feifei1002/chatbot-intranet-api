3df138dcd9724ac92c990145fcf8dce1ab2600b1
eye man ^^ <alia64@cardiff.ac.uk>
2024-04-13 16:14:45 +0100

    fixed route error

--- routes/feedback.py
+++ routes/feedback.py
@@ -6,17 +6,15 @@ from utils.db import pool
 
 router = APIRouter()
 
-router.post("/feedback")
 
-
-async def transcribe(request: Request):
+@router.post("/feedback")
+async def feedback(request: Request):
     data = await request.body()
     message = data.get("message")
     is_positive = data.get("positive")
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("SELECT id FROM messages WHERE content = %s", (message,))
-            message_id = (await cur.fetchone())
-            await cur.execute("INSERT INTO feedback (message_id, is_positive) VALUES (%s, %s)", (message_id, is_positive))
-            # hello ayman, is_positive might be a string and it might not work since the table expects a boolean
-
+            await cur.execute("INSERT INTO feedback (message_id, is_positive) "
+                              "VALUES ((SELECT id FROM messages "
+                              "WHERE content = %s),%s)", (message, is_positive))
+            
