7415ca019c4decf16e44b720b5b9981184ca3c04
Kavin Sundar <sundark@cardiff.ac.uk>
2024-04-14 19:24:21 +0100

    Configure open telemetry to send data through OTLP exporter for Newrelic.

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+import uuid
 from contextlib import asynccontextmanager
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from routes import (chat, suggested_questions, text_to_speech,
-                    conversations, admin_analytics)
-from honeycomb.opentelemetry import configure_opentelemetry
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from routes import authentication, deepgram_transcriber
-
+from routes import (chat, suggested_questions, text_to_speech,
+                    conversations, admin_analytics)
 from utils import db
 
-configure_opentelemetry()
+OTEL_RESOURCE_ATTRIBUTES = {
+    "service.instance.id": str(uuid.uuid1()),
+    "environment": "local"
+}
+
+# Configure OpenTelemetry to send to an OTLP endpoint
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create(OTEL_RESOURCE_ATTRIBUTES)
+    )
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter())
+)
+
 HTTPXClientInstrumentor().instrument()
 
 
