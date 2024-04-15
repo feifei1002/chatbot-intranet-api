083d55776051b436c8f890ccbce1601665fb8382
eye man ^^ <alia64@cardiff.ac.uk>
2024-04-16 00:19:17 +0100

    comments

--- routes/feedback.py
+++ routes/feedback.py
@@ -11,11 +11,13 @@ class FeedbackData(BaseModel):
     positive: bool
     feedback: str
 
-
+# adds content from feedback payload to the feedback table
 @router.post("/feedback")
 async def feedback(data: FeedbackData):
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
+            # INSERTs data into table. ON CONFLICT is for if a message already
+            # has feedback. it will UPDATE instead of INSERT
             await cur.execute("INSERT INTO feedback "
                               "(message_id, is_positive, written_feedback) "
                               "VALUES (%s, %s, %s) "
