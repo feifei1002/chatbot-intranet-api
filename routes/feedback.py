8720e22d2792f0dff6b86d0e52858cc0cb7348cf
eye man ^^ <alia64@cardiff.ac.uk>
2024-04-16 00:04:54 +0100

    fixed lint?

--- routes/feedback.py
+++ routes/feedback.py
@@ -1,23 +1,25 @@
-
 from fastapi import APIRouter
 from pydantic import BaseModel
 
 from utils.db import pool
 
-
 router = APIRouter()
+
 
 class FeedbackData(BaseModel):
     id: str
     positive: bool
     feedback: str
 
+
 @router.post("/feedback")
 async def feedback(data: FeedbackData):
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("INSERT INTO feedback (message_id, is_positive, written_feedback) "
+            await cur.execute("INSERT INTO feedback "
+                              "(message_id, is_positive, written_feedback) "
                               "VALUES (%s, %s, %s) "
                               "ON CONFLICT (message_id) DO UPDATE "
-                              "SET is_positive = EXCLUDED.is_positive, written_feedback = EXCLUDED.written_feedback",
-                              (data.id, data.positive, data.feedback),)
+                              "SET is_positive = EXCLUDED.is_positive, "
+                              "written_feedback = EXCLUDED.written_feedback",
+                              (data.id, data.positive, data.feedback), )
