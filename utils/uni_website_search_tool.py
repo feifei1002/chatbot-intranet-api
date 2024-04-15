80d448da0def783fdfe659c5c4677bdc1065e4b3
Kavin Sundar <sundark@cardiff.ac.uk>
2024-04-15 12:59:33 +0100

    Fix lint issues.

--- utils/uni_website_search_tool.py
+++ utils/uni_website_search_tool.py
@@ -2,8 +2,6 @@ import json
 import os
 import asyncio
 
-from llama_index.core.ingestion import IngestionPipeline
-from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.schema import MetadataMode, NodeWithScore
 from llama_index.postprocessor.cohere_rerank import CohereRerank
 
